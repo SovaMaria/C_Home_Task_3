a476f646d012abf64c02f54c0018a8f2d4ae9c8b
M_Sova <MyMail@mail.ru>
2022-11-24 18:42:18 +0300

    task 19_Palindrom

--- Task_19_Palindrom/Program.cs
+++ Task_19_Palindrom/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool Palindrom ()
+{
+    int num1 = number % 10;
+    int num2 = number % 100 / 10;
+    int num3 = number % 1000 / 100;
+    int num4 = number % 10000 / 1000;
+    int num5 = number / 10000;
+     
+    return (num1 == num5 && num2 ==num4);
+}
+Console.WriteLine(Palindrom() ? "Данное число палиндром" : "Число не является палиндромом");
